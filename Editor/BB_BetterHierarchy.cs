9830f4708a40f0b9968490a86071faf172f65900
Janusz Smoła <smola.janusz@gmail.com>
2017-05-04 15:00:41 +0200

    code refactor

--- Editor/BB_BetterHierarchy.cs
+++ Editor/BB_BetterHierarchy.cs
@@ -5,80 +5,56 @@ using UnityEditor;
 
 namespace BB_Extensions {
 
-  [InitializeOnLoad]
-  public class BB_BetterHierarchy {
-    static Texture csScriptIcon;
-    static Texture lightIcon;
-    static Texture meshRendererIcon;
-    static Texture cameraIcon;
-    static Texture particleSystemIcon;
+    [InitializeOnLoad]
+    public class BB_BetterHierarchy {
+        static Dictionary<string, Texture> icons = new Dictionary<string, Texture>()
+        {
+            {"Light", EditorGUIUtility.ObjectContent(null, typeof(Light)).image},
+            {"MonoBehaviour", EditorGUIUtility.IconContent("cs Script Icon").image},
+            {"ParticleSystem", EditorGUIUtility.ObjectContent(null, typeof(ParticleSystem)).image},
+            {"Camera", EditorGUIUtility.IconContent("Camera Icon").image},
+            {"MeshRenderer", EditorGUIUtility.ObjectContent(null, typeof(MeshRenderer)).image}
+        };
 
-    static BB_BetterHierarchy() {
-      EditorApplication.hierarchyWindowItemOnGUI = BetterHierarchy;
+        static int iconWidth = 18;
+        static int iconHeight = 18;
+        static int marginBetweenIcons = 24;
 
-      csScriptIcon = EditorGUIUtility.IconContent("cs Script Icon").image;
-      lightIcon = EditorGUIUtility.ObjectContent(null, typeof(Light)).image;
-      meshRendererIcon = EditorGUIUtility.ObjectContent(null, typeof(MeshRenderer)).image;
-      cameraIcon = EditorGUIUtility.IconContent("Camera Icon").image;
-      particleSystemIcon = EditorGUIUtility.ObjectContent(null, typeof(ParticleSystem)).image;   
+        static BB_BetterHierarchy() {
+            EditorApplication.hierarchyWindowItemOnGUI = BetterHierarchy;
+        }
+
+        static void BetterHierarchy(int instanceID, Rect selectedRect) {
+            GameObject gameobject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            if (gameobject != null)
+            {
+                Rect setActiveButtonRect = new Rect(selectedRect);
+                setActiveButtonRect.x = setActiveButtonRect.width;
+                setActiveButtonRect.width = iconWidth;
+                setActiveButtonRect.height = iconHeight;
+                gameobject.SetActive(GUI.Toggle(setActiveButtonRect, gameobject.activeSelf, ""));
+
+                int iconCount = 0;
+                foreach (KeyValuePair<string, Texture> bbIcon in icons)
+                {
+                    iconCount += 1;
+                    if (gameobject.GetComponent(bbIcon.Key) != null)
+                    {
+                        displayIcon(bbIcon, marginBetweenIcons * iconCount, selectedRect);
+                    }
+                }
+            }
+        }
+
+        static void displayIcon(KeyValuePair<string, Texture> bbIcon, int position, Rect selectedRect) {
+            Rect gameobjectIconRect = new Rect();
+            gameobjectIconRect.x = selectedRect.width - position;
+            gameobjectIconRect.y = selectedRect.y;
+            gameobjectIconRect.width = iconWidth;
+            gameobjectIconRect.height = iconHeight;
+            GUI.Label(gameobjectIconRect, bbIcon.Value);
+        }
     }
-
-    static void BetterHierarchy(int instanceID, Rect selectionRect) {
-      GameObject gameobject = EditorUtility.InstanceIDToObject (instanceID) as GameObject;
-
-      if (gameobject != null) {
-        Rect setActiveButtonRect = new Rect(selectionRect); 
-        setActiveButtonRect.x = setActiveButtonRect.width;
-        setActiveButtonRect.width = 18;
-        setActiveButtonRect.height = 18;
-        gameobject.SetActive(GUI.Toggle(setActiveButtonRect, gameobject.activeSelf, ""));
-
-        if (gameobject.GetComponent<Light>() != null) {
-          Rect gameobjectIconRect = new Rect();
-          gameobjectIconRect.x = selectionRect.width - 24;
-          gameobjectIconRect.y = selectionRect.y;
-          gameobjectIconRect.width = 18;
-          gameobjectIconRect.height = 18;
-          GUI.Label(gameobjectIconRect, lightIcon);
-        }
-
-        if (gameobject.GetComponent<MonoBehaviour>() != null) {
-          Rect gameobjectIconRect = new Rect();
-          gameobjectIconRect.x = selectionRect.width - (24 * 2);
-          gameobjectIconRect.y = selectionRect.y;
-          gameobjectIconRect.width = 18;
-          gameobjectIconRect.height = 18;
-          GUI.Label(gameobjectIconRect, csScriptIcon);
-        }
-
-        if (gameobject.GetComponent<MeshRenderer>() != null) {
-          Rect gameobjectIconRect = new Rect();
-          gameobjectIconRect.x = selectionRect.width - (24 * 3);
-          gameobjectIconRect.y = selectionRect.y;
-          gameobjectIconRect.width = 18;
-          gameobjectIconRect.height = 18;
-          GUI.Label(gameobjectIconRect, meshRendererIcon);
-        }
-
-        if (gameobject.GetComponent<Camera>() != null) {
-          Rect gameobjectIconRect = new Rect();
-          gameobjectIconRect.x = selectionRect.width - (24 * 4);
-          gameobjectIconRect.y = selectionRect.y;
-          gameobjectIconRect.width = 18;
-          gameobjectIconRect.height = 18;
-          GUI.Label(gameobjectIconRect, cameraIcon);
-        }
-
-        if (gameobject.GetComponent<ParticleSystem>() != null) {
-          Rect gameobjectIconRect = new Rect();
-          gameobjectIconRect.x = selectionRect.width - (24 * 5);
-          gameobjectIconRect.y = selectionRect.y;
-          gameobjectIconRect.width = 18;
-          gameobjectIconRect.height = 18;
-          GUI.Label(gameobjectIconRect, particleSystemIcon);
-        }
-      }
-    }
-  }
 
 }
